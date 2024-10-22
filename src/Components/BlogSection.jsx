8318ef4e5f7f3cb48d522145e9c6af6e6df02e32
viral <viral@viraldevmurari.com>
2024-10-22 07:20:59 +0530

    Fetch the blog home page

--- src/Components/BlogSection.jsx
+++ src/Components/BlogSection.jsx
@@ -1,26 +1,21 @@
-const BlogSection = () => {
+const BlogSection = (props) => {
+  console.log(props.data[0]);
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="container px-6 py-10 mx-auto">
-        <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
-          From the blog
-        </h1>
-
         <div className="mt-8 lg:-mx-6 lg:flex lg:items-center">
           <img
-            className="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96"
+            className="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-34"
             src="https://images.unsplash.com/photo-1590283603385-17ffb3a7f29f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
             alt="Blog Cover"
           />
 
           <div className="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6">
-            <p className="text-sm text-blue-500 uppercase">category</p>
-
             <a
               href="#"
               className="block mt-4 text-2xl font-semibold text-gray-800 hover:underline dark:text-white md:text-3xl"
             >
-              All the features you want to know
+              {props.data[0].title}
             </a>
 
             <p className="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from "react";
 import BlogSection from "./BlogSection";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://localhost:7211/api/Blog").then((response) =>
+      response.json().then((data) => {
+        // console.log(data);
+        setData(data);
+      })
+    );
+  }, []);
   return (
     <>
-      <BlogSection />
-      <BlogSection />
+      <BlogSection data={data} />
+      {/* <BlogSection /> */}
     </>
   );
 }
